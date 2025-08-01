5524610ed79220ddddb4a63b17b30b2d137c0d9f
CreativeNewEra <149348461+CreativeNewEra@users.noreply.github.com>
2025-07-31 20:32:26 -0400

    Add logging configuration utility

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from controllers.main_controller import MainController
+from utils.logging_config import setup_logging
 
+
+setup_logging()
 
 if __name__ == "__main__":
     controller = MainController()
     controller.run()
-
